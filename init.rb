015a8c3ba59396284b580dceb9ac54a10bdb15ff
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2022-05-10 11:23:20 +0200

    improved formatting

--- init.rb
+++ init.rb
@@ -8,7 +8,14 @@ Redmine::Plugin.register :plantuml do
   requires_redmine version: '5.0'
 
   settings(partial: 'settings/plantuml',
-           default: { 'convert_by' => 'server', 'server_url' => 'http://www.plantuml.com', 'plantuml_binary' => '/usr/local/bin', 'cache_seconds' => '0', 'allow_includes' => false })
+           default: { 
+             'convert_by' => 'server',
+             'server_url' => 'http://www.plantuml.com',
+             'plantuml_binary' => '/usr/local/bin',
+             'cache_seconds' => '0',
+             'allow_includes' => false
+           }
+          )
 
   Redmine::WikiFormatting::Macros.register do
     desc <<EOF
