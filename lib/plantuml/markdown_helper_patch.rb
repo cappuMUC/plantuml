86acc780b3e41c53202c9b9a7496a108f349f341
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2022-05-11 15:39:40 +0200

    refactored helper patches for zeitwerk support

--- lib/plantuml/markdown_helper_patch.rb
+++ lib/plantuml/markdown_helper_patch.rb
@@ -1,25 +1,26 @@
 require_dependency 'redmine/wiki_formatting/markdown/helper'
+module Plantuml
+  module MarkdownHelperPatch
+    def self.included(base) # :nodoc:
+      base.send(:prepend, HelperMethodsWikiExtensions)
 
-module PlantumlMarkdownHelperPatch
-  def self.included(base) # :nodoc:
-    base.send(:prepend, HelperMethodsWikiExtensions)
-
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+      end
     end
   end
-end
 
-module HelperMethodsWikiExtensions
-  # extend the editor Toolbar for adding a plantuml button
-  # overwrite this helper method to have full control about the load order
-  def heads_for_wiki_formatter
-    return if @heads_for_wiki_plantuml_included
-    super
-    content_for :header_tags do
+  module HelperMethodsWikiExtensions
+    # extend the editor Toolbar for adding a plantuml button
+    # overwrite this helper method to have full control about the load order
+    def heads_for_wiki_formatter
+      return if @heads_for_wiki_plantuml_included
+      super
+      content_for :header_tags do
         javascript_include_tag('plantuml.js', plugin: 'plantuml') +
-        stylesheet_link_tag('plantuml.css', plugin: 'plantuml')
+          stylesheet_link_tag('plantuml.css', plugin: 'plantuml')
+      end
+      @heads_for_wiki_plantuml_included = true
     end
-    @heads_for_wiki_plantuml_included = true
   end
 end
--- lib/plantuml/textile_helper_patch.rb
+++ lib/plantuml/textile_helper_patch.rb
@@ -1,25 +1,27 @@
 require_dependency 'redmine/wiki_formatting/textile/helper'
 
-module PlantumlTextileHelperPatch
-  def self.included(base) # :nodoc:
-    base.send(:prepend, HelperMethodsWikiExtensions)
+module Plantuml
+  module TextileHelperPatch
+    def self.included(base) # :nodoc:
+      base.send(:prepend, HelperMethodsWikiExtensions)
 
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+      end
     end
   end
-end
 
-module HelperMethodsWikiExtensions
-  # extend the editor Toolbar for adding a plantuml button
-  # overwrite this helper method to have full control about the load order
-  def heads_for_wiki_formatter
-    return if @heads_for_wiki_plantuml_included
-    super
-    content_for :header_tags do
+  module HelperMethodsWikiExtensions
+    # extend the editor Toolbar for adding a plantuml button
+    # overwrite this helper method to have full control about the load order
+    def heads_for_wiki_formatter
+      return if @heads_for_wiki_plantuml_included
+      super
+      content_for :header_tags do
         javascript_include_tag('plantuml.js', plugin: 'plantuml') +
-        stylesheet_link_tag('plantuml.css', plugin: 'plantuml')
+          stylesheet_link_tag('plantuml.css', plugin: 'plantuml')
+      end
+      @heads_for_wiki_plantuml_included = true
     end
-    @heads_for_wiki_plantuml_included = true
   end
 end
